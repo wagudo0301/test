5315c87e3910924a598b0d1174d09cd95a5b6ddc
PCHM750PAW\riku0 <riku02052225@gmail.com>
2021-11-09 16:07:44 +0900

    キャラ動かすテスト

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -2,32 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-
-    //¡‰ñ‚â‚è‚½‚¢‚±‚Æ
-    //•Ï”ì¬@(ˆÚ“®ƒXƒs[ƒhAƒAƒjƒ[ƒVƒ‡ƒ“A“·‘Ì)
-    [SerializeField,Tooltip("ˆÚ“®ƒXƒs[ƒh")]
+    [SerializeField, Tooltip("ç§»å‹•ã‚¹ãƒ”ãƒ¼ãƒ‰")]
     private int moveSpeed;
-
     [SerializeField]
     private Animator playerAnim;
-
-    public Rigidbody2D rb;
-
-
-    //ƒL[“ü—Í‚ğó‚¯æ‚èˆÚ“®‚³‚¹‚éƒR[ƒh‹Lq
-    //•ûŒü‚ÆƒAƒjƒ[ƒVƒ‡ƒ“‚ğ˜A“®
-    
+    public Rigidbody2D Rb;
     // Start is called before the first frame update
     void Start()
     {
-     
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical") * moveSpeed);
     }
 }
